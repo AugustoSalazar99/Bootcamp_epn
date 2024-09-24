90dec43f0f6acb7caff4e10339f257e26e93cfd4
augusto99 <augusto31115@gmail.com>
2024-09-23 21:46:59 -0500

    consumir api

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,11 +1,32 @@
-export function PokemonCard(props:{name : string; url:string}){
-    // usar prosp
-    return (
-        <div>
-         <p >{props.name} </p>
-         <p >{props.url}</p>
-         
+export function PokemonCard(props: { name: string; url: string }) {
+  const { name, url } = props;
+
+  // usar prosp
+  const pokemonId = url.slice(0,-1).split('/').pop();
+  return (
     
+    //   {/* hay otra forma de hacerlo
+    //      {props.name} {props.url}
+        
+    //   {/* <p>{name}</p>
+    //   <p>{url}</p> */}
+
+      <div className="card border bg-base-100 col-span-4 shadow-xl">
+        <figure>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`} 
+            alt="Shoes"
+            className="size-[200px]"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name.charAt(0).toUpperCase()+name.slice(1)}</h2>
+          {/* <p>{url}</p> */}
+          <p>Pokedex ID: {pokemonId}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Ver Pokemon</button>
+          </div>
         </div>
-    );
+      </div>
+  );
 }
